fd7ee3d0c42645fa631938fbd17995edbf7e0bf6
1yoouoo <1yoouoo@gmail.com>
2023-04-07 14:11:13 +0900

    style: Update LoginPage

--- src/pages/LoginPage/index.tsx
+++ src/pages/LoginPage/index.tsx
@@ -1,7 +1,12 @@
+import LoginForm from '../../components/LoginForm';
 import './style.scss';
 
 const LoginPage = () => {
-  return <div className='LoginPage' />;
+  return (
+    <div className='LoginPage'>
+      <LoginForm />
+    </div>
+  );
 };
 
 export default LoginPage;
