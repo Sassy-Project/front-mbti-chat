6876dc05f0773a6a7871bf05723eb7a6362ef07b
1yoouoo <1yoouoo@gmail.com>
2023-04-07 15:58:11 +0900

    feat: Add useEffect for dark theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
+import { useEffect } from 'react';
 import LoginPage from './pages/LoginPage';
 
 const App = () => {
+  useEffect(() => {
+    document.documentElement.setAttribute('data-theme', 'light');
+  }, []);
   return (
     <div>
       <LoginPage />
