1e4ca4746377479eb2b1b2a7199387c358787b68
1yoouoo <1yoouoo@gmail.com>
2023-04-07 14:26:31 +0900

    feat: Update button

---
+++ src/components/styled-components/Button.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const Button = () => {
+  return <StyledButton>button</StyledButton>;
+};
+
+const StyledButton = styled.div`
+  background-color: red;
+`;
+export default Button;
