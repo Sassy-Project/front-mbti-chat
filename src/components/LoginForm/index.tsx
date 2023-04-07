5d75064b5527377cd9fc3b2d9e75e0a4381ef184
1yoouoo <1yoouoo@gmail.com>
2023-04-07 15:59:40 +0900

    feat: Add func for testing

--- src/components/LoginForm/index.tsx
+++ src/components/LoginForm/index.tsx
@@ -2,10 +2,16 @@ import Button from '../styled-components/Button';
 import './style.scss';
 
 const LoginForm = () => {
+  const onClickEvent = () => {
+    // eslint-disable-next-line no-console
+    console.log('You clicked !');
+  };
   return (
     <div className='LoginForm'>
       <form>이메일</form>
-      <Button />
+
+      <Button text='이메일 로그인' onClick={onClickEvent} />
+      <Button text='테스트' onClick={onClickEvent} color='var(--color-background)' />
     </div>
   );
 };
