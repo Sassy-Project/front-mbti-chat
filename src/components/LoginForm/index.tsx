0ffb7c193e9eae799d32821cb0bd184e7949fa35
ydj <motokrzr11@gmail.com>
2023-04-09 17:48:34 +0900

    feat: 로그인페이지 아이디/패스워드 추가하였습니다.

--- src/components/LoginForm/index.tsx
+++ src/components/LoginForm/index.tsx
@@ -1,4 +1,5 @@
 import Button from '../styled-components/Button';
+import { Link } from 'react-router-dom';
 import './style.scss';
 
 const LoginForm = () => {
@@ -8,10 +9,27 @@ const LoginForm = () => {
   };
   return (
     <div className='LoginForm'>
-      <form>이메일</form>
-
-      <Button text='이메일 로그인' onClick={onClickEvent} />
-      <Button text='테스트' onClick={onClickEvent} color='var(--color-background)' />
+      <form>
+        <div className='BasicInput'>
+          <label htmlFor='loginId'>아이디</label>
+          <input id='loginId' type='text' placeholder='아이디를 입력해주세요.' />
+          {true && <span className='BasicInput__error'>아이디를 확인해주세요.</span>}
+        </div>
+        <div className='BasicInput'>
+          <label htmlFor='loginPw'>패스워드</label>
+          <input id='loginPw' type='text' placeholder='비밀번호 8자 이하로 입력해주세요.' />
+          {true && <span className='BasicInput__error'>패스워드를 확인해주세요.</span>}
+        </div>
+        <Button text='로그인' onClick={onClickEvent} />
+        <div className='LoginSupport'>
+          <Link to='/login' style={{ textDecoration: 'none' }}>
+            비밀번호 찾기
+          </Link>
+          <Link to='/login' style={{ textDecoration: 'none' }}>
+            회원가입 하기
+          </Link>
+        </div>
+      </form>
     </div>
   );
 };
