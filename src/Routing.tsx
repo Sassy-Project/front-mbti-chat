8f87192bae02445310ce3806ad523ddbd9a66ddd
1yoouoo <1yoouoo@gmail.com>
2023-04-09 14:04:19 +0900

    fix: Move Header to Router from App

--- src/Routing.tsx
+++ src/Routing.tsx
@@ -2,9 +2,12 @@ import React from 'react';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import MainPage from './pages/MainPage';
+import Header from './common/Header';
 
 const Routing: React.FC = (): JSX.Element => (
   <BrowserRouter>
+    <Header />
+
     <Routes>
       <Route path='/login' element={<LoginPage />} />
       <Route path='/*' element={<MainPage />} />
