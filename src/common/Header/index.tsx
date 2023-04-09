7af0f7bcc713029b203764b6912397f3f135a727
1yoouoo <1yoouoo@gmail.com>
2023-04-09 14:04:52 +0900

    feat: Add navigate func

--- src/common/Header/index.tsx
+++ src/common/Header/index.tsx
@@ -1,7 +1,9 @@
+import { useNavigate } from 'react-router-dom';
 import Button from '../../components/styled-components/Button';
 import './style.scss';
 
 const Header = () => {
+  const navigate = useNavigate();
   return (
     <header className='Header'>
       <section className='Header__nav'>
@@ -9,13 +11,13 @@ const Header = () => {
         <div className='Header__nav--right'>
           <Button
             text='로그인'
-            onClick={() => console.log()}
+            onClick={() => navigate('/login')}
             width='88px'
             height='40px'
             color='#000'
             background='#fff'
           />
-          <Button text='회원가입' onClick={() => console.log()} width='88px' height='40px' />
+          <Button text='회원가입' onClick={() => navigate('/signup')} width='88px' height='40px' />
         </div>
       </section>
     </header>
