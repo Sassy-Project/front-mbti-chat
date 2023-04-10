d342d1b67521473cf255aec963c8f8d6133d3c06
1yoouoo <1yoouoo@gmail.com>
2023-04-10 16:57:01 +0900

    docs: Add test routing

---
+++ src/pages/TestPage/index.tsx
@@ -0,0 +1,12 @@
+import Carousel from '../../components/Carousel';
+import './style.scss';
+
+const TestPage = () => {
+  return (
+    <div className='TestPage'>
+      <Carousel />
+    </div>
+  );
+};
+
+export default TestPage;
