29b0f04cc852ce9535ff13a7aa02e35931d38344
danakim530 <danakim530@gmail.com>
2023-04-18 00:41:22 +0900

    fix: MbtiPage

--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -5,10 +5,11 @@ import StartTest from '../../components/MbtiTest/startTest';
 const MainPage = () => {
   return (
     <div className={styles.MainPage}>
-      <div className={styles.MainPage__chatStart}>MBTI 채팅 바로시작</div>
+      <div className={styles.MainPage__title}>MBTI 채팅 바로시작</div>
       <div className={styles.MainPage__carousel}>
         <Carousel />
       </div>
+      <div className={styles.MainPage__title}>MBTI 테스트하기</div>
       <StartTest />
     </div>
   );
