662ac70432391448580a704b96caf38936469008
1yoouoo <1yoouoo@gmail.com>
2023-04-23 18:31:34 +0900

    feat: Add ChatPage Routes

--- src/Routing.tsx
+++ src/Routing.tsx
@@ -8,14 +8,16 @@ import ProfilePage from './pages/ProfilePage';
 import SignUpPage from './pages/SignUpPage';
 import MbtiTestPage from './pages/MainPage/MbtiTestPage/index';
 import MbtiTestResult from './pages/MainPage/MbtiTestResult/index';
+import ChatPage from './pages/ChatPage';
+// MbtiProvider를 routes 전체를 감싸지 않는 방법을 찾아야함
 
 const Routing: React.FC = (): JSX.Element => (
   <BrowserRouter>
     <Header />
-
     <Routes>
       <Route path='/login' element={<LoginPage />} />
       <Route path='/*' element={<MainPage />} />
+      <Route path='/chat/:userId' element={<ChatPage />} />
       <Route path='/mbtitest' element={<MbtiTestPage />} />
       <Route path='/mbtitestresult' element={<MbtiTestResult />} />
       <Route path='/users/:userId' element={<ProfilePage />} />
