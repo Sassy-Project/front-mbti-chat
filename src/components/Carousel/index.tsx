28076e84e9c2d13ed5cf8d7b4f0a1f68aee31975
1yoouoo <1yoouoo@gmail.com>
2023-04-23 18:32:09 +0900

    feat: Update Carousel 배열-> 배열+객체

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -1,39 +1,66 @@
 /* eslint-disable no-console */
-import { useState } from 'react';
+import { useContext, useState } from 'react';
 import MbtiCard, { Card } from '../styled-components/MbtiCard';
 import styles from './style.module.scss';
-
-const mbtiList = [
-  'ESTP',
-  'ESFP',
-  'ENFP',
-  'ENTP',
-  'ESTJ',
-  'ESFJ',
-  'ENFJ',
-  'ENTJ',
-  'ISTJ',
-  'ISFJ',
-  'INFJ',
-  'INTJ',
-  'ISTP',
-  'ISFP',
-  'INFP',
-  'INTP',
-];
+import { MbtiContext } from '../../Context/MbtiContext';
+import { useNavigate } from 'react-router-dom';
 
 const Carousel = () => {
+  const { selectMbti } = useContext(MbtiContext);
+  const navigate = useNavigate();
+  const userId = localStorage.getItem('id');
+
+  const [mbtiList, setMbtiList] = useState([
+    { id: 1, mbti: 'ESTP', isSelected: false },
+    { id: 2, mbti: 'ESFP', isSelected: false },
+    { id: 3, mbti: 'ENFP', isSelected: false },
+    { id: 4, mbti: 'ENTP', isSelected: false },
+    { id: 5, mbti: 'ESTJ', isSelected: false },
+    { id: 6, mbti: 'ESFJ', isSelected: false },
+    { id: 7, mbti: 'ENFJ', isSelected: false },
+    { id: 8, mbti: 'ENTJ', isSelected: false },
+    { id: 9, mbti: 'ISTJ', isSelected: false },
+    { id: 10, mbti: 'ISFJ', isSelected: false },
+    { id: 11, mbti: 'INFJ', isSelected: false },
+    { id: 12, mbti: 'INTJ', isSelected: false },
+    { id: 13, mbti: 'ISTP', isSelected: false },
+    { id: 14, mbti: 'ISFP', isSelected: false },
+    { id: 15, mbti: 'INFP', isSelected: false },
+    { id: 16, mbti: 'INTP', isSelected: false },
+  ]);
   const [selectedCard, setSelectedCard] = useState<Card>({
-    mbti: 'ESTP', // 초기값
-    index: 0,
+    mbti: '',
+    id: 0,
   });
   const [stackIndex, setStackIndex] = useState<number>(0);
 
-  const onClickCard = (mbti: string) => {
-    const matchingCardIndex = mbtiList.indexOf(mbti);
-    const relativeIndex = calculateRelativeIndex(selectedCard.index, matchingCardIndex);
-    setSelectedCard({ mbti, index: matchingCardIndex });
+  const onSpinCard = (mbti: string) => {
+    const matchingCard = mbtiList.find((card) => card.mbti === mbti);
+    const relativeIndex = calculateRelativeIndex(selectedCard.id - 1, matchingCard.id - 1);
     setStackIndex((prevStackIndex) => prevStackIndex + relativeIndex);
+
+    return matchingCard;
+  };
+
+  const updateMbtiList = (matchingCard) => {
+    const updatedMbtiList = mbtiList.map((card) =>
+      card.id === matchingCard.id ? { ...card, isSelected: true } : { ...card, isSelected: false }
+    );
+    setMbtiList(updatedMbtiList);
+  };
+
+  const onClickCard = (mbti: string) => {
+    if (mbtiList.some((card) => card.isSelected && card.mbti === mbti)) {
+      userId ? navigate(`/chat/${userId}`) : alert('로그인 해주세요');
+    }
+    // 빙글빙글 담당
+    const matchingCard = onSpinCard(mbti);
+    // 로컬 상태 업데이트
+    updateMbtiList(matchingCard);
+    // 전역 상태 업데이트
+    selectMbti(mbti);
+
+    setSelectedCard({ mbti, id: matchingCard.id, isSelected: true });
   };
 
   const calculateRelativeIndex = (currentIndex: number, matchingIndex: number): number => {
@@ -53,14 +80,15 @@ const Carousel = () => {
         }}
       >
         {mbtiList &&
-          mbtiList.map((mbti: string, index: number): JSX.Element => {
+          mbtiList.map((mbti): JSX.Element => {
             return (
               <MbtiCard
-                key={mbti}
-                mbti={mbti}
-                background={`var(--color-${mbti})`}
-                index={index}
+                key={mbti.id}
+                mbti={mbti.mbti}
+                background={`var(--color-${mbti.mbti})`}
+                id={mbti.id}
                 onClickCard={onClickCard}
+                isSelected={mbti.isSelected}
               />
             );
           })}
