bfb3669da006cbcd4ad67065b2bab09e1d478081
danakim530 <danakim530@gmail.com>
2023-04-25 17:29:50 +0900

    fix : create CloseIcon.tsx

--- src/components/Sidebar/SidebarLogin.tsx
+++ src/components/Sidebar/SidebarLogin.tsx
@@ -1,5 +1,6 @@
 import styles from './Sidebar.module.scss';
 import { Link } from 'react-router-dom';
+import CloseIcon from '../../assets/CloseIcon';
 
 const SidebarLogin = () => {
   const userNickname = localStorage.getItem('nickname');
@@ -13,19 +14,7 @@ const SidebarLogin = () => {
       <div className={styles.Sidebar__background} />
       <div className={styles.Sidebar}>
         <div className={styles.Sidebar__CloseIcon}>
-          <svg
-            xmlns='http://www.w3.org/2000/svg'
-            width='16'
-            height='16'
-            fill='currentColor'
-            viewBox='0 0 16 16'
-          >
-            <path d='M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z' />
-          </svg>
-          <path
-            fillRule='evenodd'
-            d='M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z'
-          />
+          <CloseIcon />
         </div>
         <ul className={styles.Sidebar__content}>
           <li className={styles.Sidebar__content__letter1}>
