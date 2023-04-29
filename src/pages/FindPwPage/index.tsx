f858dc0be11886df73a90379961fc655e6eca295
ydj <motokrzr11@gmail.com>
2023-04-29 17:38:33 +0900

    feat: find Pw, New Pw

---
+++ src/pages/FindPwPage/index.tsx
@@ -0,0 +1,12 @@
+import styles from './style.module.scss';
+import FindPwForm from '../../components/FindPwForm';
+
+const FindPwPage = () => {
+  return (
+    <div className={styles.FindPwPage}>
+      <FindPwForm />
+    </div>
+  );
+};
+
+export default FindPwPage;
